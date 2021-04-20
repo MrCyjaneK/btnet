d0546c3d52261045bd87ce988ea2c3a92a8e38cc
Czarek Nakamoto <cyjan@mrcyjanek.net>
2021-04-20 12:31:59 +0200

    Add missing HOST= for cross compiling

--- dist/build.sh
+++ dist/build.sh
@@ -21,18 +21,18 @@ do
     BINNAME="btnet-$name"
     echo "/ Linux builds - daemon."
     echo -n -e "|- bin/$BINNAME-$name""_linux_386 "
-    CGO_ENABLED=1 CC=i686-linux-gnu-gcc CXX=i686-linux-gnu-g++ GOOS=linux GOARCH=386 go build -o bin/"$BINNAME"_linux_386 $addon && ok
+    CGO_ENABLED=1 HOST=i686-linux-gnu CC=i686-linux-gnu-gcc CXX=i686-linux-gnu-g++ GOOS=linux GOARCH=386 go build -o bin/"$BINNAME"_linux_386 $addon && ok
     echo -n -e "|- bin/$BINNAME-$name""_linux_amd64 "
     CGO_ENABLED=1 GOOS=linux GOARCH=amd64   go build -o bin/"$BINNAME"_linux_amd64 -tags gui ../../ && ok
     echo -n -e "|- bin/$BINNAME-$name""_linux_arm "
-    CGO_ENABLED=1 CC=arm-linux-gnueabi-gcc CXX=arm-linux-gnueabi-g++ GOOS=linux GOARCH=arm go build -o bin/"$BINNAME"_linux_arm $addon && ok
+    CGO_ENABLED=1 HOST=arm-linux-gnueabi CC=arm-linux-gnueabi-gcc CXX=arm-linux-gnueabi-g++ GOOS=linux GOARCH=arm go build -o bin/"$BINNAME"_linux_arm $addon && ok
     echo -n -e "\_ bin/$BINNAME-$name""_linux_arm64 "
-    CGO_ENABLED=1 CC=aarch64-linux-gnu-gcc CXX=aarch64-linux-gnu-g++ GOOS=linux GOARCH=arm64 go build -o bin/"$BINNAME"_linux_arm64 $addon && ok
+    CGO_ENABLED=1 HOST=aarch64-linux-gnu CC=aarch64-linux-gnu-gcc CXX=aarch64-linux-gnu-g++ GOOS=linux GOARCH=arm64 go build -o bin/"$BINNAME"_linux_arm64 $addon && ok
     echo "/ Windows builds - daemon"
     echo -n -e "|- bin/$BINNAME-$name""_windows_386.exe "
-    CGO_ENABLED=1 CC=i686-w64-mingw32-gcc CXX=i686-w64-mingw32-g++ GOOS=windows GOARCH=386 go build -o bin/"$BINNAME"_windows_386.exe $addon && ok
+    CGO_ENABLED=1 HOST=i686-w64-mingw32 CC=i686-w64-mingw32-gcc CXX=i686-w64-mingw32-g++ GOOS=windows GOARCH=386 go build -o bin/"$BINNAME"_windows_386.exe $addon && ok
     echo -n -e "|- bin/$BINNAME-$name""_windows_amd64.exe "
-    CGO_ENABLED=1 CC=x86_64-w64-mingw32-gcc CXX=x86_64-w64-mingw32-g++ GOOS=windows GOARCH=amd64 go build -o bin/"$BINNAME"_windows_amd64.exe $addon && ok
+    CGO_ENABLED=1 HOST=x86_64-w64-mingw32 CC=x86_64-w64-mingw32-gcc CXX=x86_64-w64-mingw32-g++ GOOS=windows GOARCH=amd64 go build -o bin/"$BINNAME"_windows_amd64.exe $addon && ok
     for arch in arm64 arm amd64 386
     do
         case "$arch" in
@@ -65,18 +65,18 @@ cd $builddir
 BINNAME="btnet"
 echo "/ Linux builds - daemon."
 echo -n -e "|- bin/$BINNAME""_linux_386 "
-CGO_ENABLED=1 CC=i686-linux-gnu-gcc CXX=i686-linux-gnu-g++ GOOS=linux GOARCH=386 go build -o bin/"$BINNAME"_linux_386 ../../ && ok
+CGO_ENABLED=1 HOST=i686-linux-gnu CC=i686-linux-gnu-gcc CXX=i686-linux-gnu-g++ GOOS=linux GOARCH=386 go build -o bin/"$BINNAME"_linux_386 ../../ && ok
 echo -n -e "|- bin/$BINNAME""_linux_amd64 "
 CGO_ENABLED=1 GOOS=linux GOARCH=amd64 go build -o bin/jwstudy_linux_amd64 ../../ && ok
 echo -n -e "|- bin/$BINNAME""_linux_arm "
-CGO_ENABLED=1 CC=arm-linux-gnueabi-gcc CXX=arm-linux-gnueabi-g++ GOOS=linux GOARCH=arm go build -o bin/"$BINNAME"_linux_arm ../../ && ok
+CGO_ENABLED=1 HOST=arm-linux-gnueabi CC=arm-linux-gnueabi-gcc CXX=arm-linux-gnueabi-g++ GOOS=linux GOARCH=arm go build -o bin/"$BINNAME"_linux_arm ../../ && ok
 echo -n -e "|_ bin/$BINNAME""_linux_arm64 "
-CGO_ENABLED=1 CC=aarch64-linux-gnu-gcc CXX=aarch64-linux-gnu-g++ GOOS=linux GOARCH=arm64 go build -o bin/"$BINNAME"_linux_arm64 ../../ && ok
+CGO_ENABLED=1 HOST=aarch64-linux-gnu CC=aarch64-linux-gnu-gcc CXX=aarch64-linux-gnu-g++ GOOS=linux GOARCH=arm64 go build -o bin/"$BINNAME"_linux_arm64 ../../ && ok
 echo "/ Windows builds - daemon"
 echo -n -e "|- bin/$BINNAME""_windows_386.exe "
-CGO_ENABLED=1 CC=i686-w64-mingw32-gcc CXX=i686-w64-mingw32-g++ GOOS=windows GOARCH=386 go build -o bin/"$BINNAME"_windows_386.exe ../../ && ok
+CGO_ENABLED=1 HOST=i686-w64-mingw32 CC=i686-w64-mingw32-gcc CXX=i686-w64-mingw32-g++ GOOS=windows GOARCH=386 go build -o bin/"$BINNAME"_windows_386.exe ../../ && ok
 echo -n -e "|_ bin/$BINNAME""_windows_amd64.exe "
-CGO_ENABLED=1 CC=x86_64-w64-mingw32-gcc CXX=x86_64-w64-mingw32-g++ GOOS=windows GOARCH=amd64 go build -o bin/"$BINNAME"_windows_amd64.exe ../../ && ok
+CGO_ENABLED=1 HOST=x86_64-w64-mingw32 CC=x86_64-w64-mingw32-gcc CXX=x86_64-w64-mingw32-g++ GOOS=windows GOARCH=amd64 go build -o bin/"$BINNAME"_windows_amd64.exe ../../ && ok
 
 echo "/ Packaging for debian"
 for arch in arm64 arm amd64 386
