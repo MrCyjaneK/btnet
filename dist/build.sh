5fcca6f0fb8c380faf1c80be3f8a88681fe2c425
Czarek Nakamoto <cyjan@mrcyjanek.net>
2021-04-20 18:05:14 +0200

    Final changes to build script, everything works.

--- dist/build.sh
+++ dist/build.sh
@@ -30,9 +30,9 @@ do
     CGO_ENABLED=1 HOST=aarch64-linux-gnu CC=aarch64-linux-gnu-gcc CXX=aarch64-linux-gnu-g++ PKG_CONFIG_PATH=/usr/lib/aarch64-linux-gnu/pkgconfig/ GOOS=linux GOARCH=arm64 go build -o bin/"$BINNAME"_linux_arm64 $addon && ok
     echo "/ Windows builds - daemon"
     echo -n -e "|- bin/$BINNAME""_windows_386.exe "
-    CGO_ENABLED=1 HOST=i686-w64-mingw32 CC=i686-w64-mingw32-gcc CXX=i686-w64-mingw32-g++ GOOS=windows GOARCH=386 go build -o bin/"$BINNAME"_windows_386.exe $addon && ok
+    CGO_ENABLED=1 HOST=i686-w64-mingw32 CC=i686-w64-mingw32-gcc CXX=i686-w64-mingw32-g++ GOOS=windows GOARCH=386 go build -ldflags "-s -w -H=windowsgui -extldflags=-static" -o bin/"$BINNAME"_windows_386.exe $addon && ok
     echo -n -e "|- bin/$BINNAME""_windows_amd64.exe "
-    CGO_ENABLED=1 HOST=x86_64-w64-mingw32 CC=x86_64-w64-mingw32-gcc CXX=x86_64-w64-mingw32-g++ GOOS=windows GOARCH=amd64 go build -o bin/"$BINNAME"_windows_amd64.exe $addon && ok
+    CGO_ENABLED=1 HOST=x86_64-w64-mingw32 CC=x86_64-w64-mingw32-gcc CXX=x86_64-w64-mingw32-g++ GOOS=windows GOARCH=amd64 go build -ldflags "-s -w -H=windowsgui -extldflags=-static" -o bin/"$BINNAME"_windows_amd64.exe $addon && ok
     for arch in arm64 arm amd64 386
     do
         cd "$builddir"
@@ -68,16 +68,16 @@ echo "/ Linux builds - daemon."
 echo -n -e "|- bin/$BINNAME""_linux_386 "
 CGO_ENABLED=1 HOST=i686-linux-gnu CC=i686-linux-gnu-gcc CXX=i686-linux-gnu-g++ PKG_CONFIG_PATH=/usr/lib/i386-linux-gnu/pkgconfig/ GOOS=linux GOARCH=386 go build -o bin/"$BINNAME"_linux_386 ../../ && ok
 echo -n -e "|- bin/$BINNAME""_linux_amd64 "
-CGO_ENABLED=1 GOOS=linux GOARCH=amd64 go build -o bin/"$BITNAME"_linux_amd64 ../../ && ok
+CGO_ENABLED=1 GOOS=linux GOARCH=amd64 go build -o bin/"$BINNAME"_linux_amd64 ../../ && ok
 echo -n -e "|- bin/$BINNAME""_linux_arm "
 CGO_ENABLED=1 HOST=arm-linux-gnueabihf CC=arm-linux-gnueabihf-gcc CXX=arm-linux-gnueabihf-g++ PKG_CONFIG_PATH=/usr/lib/arm-linux-gnueabihf/pkgconfig/ GOOS=linux GOARCH=arm go build -o bin/"$BINNAME"_linux_arm ../../ && ok
 echo -n -e "|_ bin/$BINNAME""_linux_arm64 "
 CGO_ENABLED=1 HOST=aarch64-linux-gnu CC=aarch64-linux-gnu-gcc CXX=aarch64-linux-gnu-g++ PKG_CONFIG_PATH=/usr/lib/aarch64-linux-gnu/pkgconfig/ GOOS=linux GOARCH=arm64 go build -o bin/"$BINNAME"_linux_arm64 ../../ && ok
 echo "/ Windows builds - daemon"
 echo -n -e "|- bin/$BINNAME""_windows_386.exe "
-CGO_ENABLED=1 HOST=i686-w64-mingw32 CC=i686-w64-mingw32-gcc CXX=i686-w64-mingw32-g++ GOOS=windows GOARCH=386 go build -o bin/"$BINNAME"_windows_386.exe ../../ && ok
+CGO_ENABLED=1 HOST=i686-w64-mingw32 CC=i686-w64-mingw32-gcc CXX=i686-w64-mingw32-g++ GOOS=windows GOARCH=386 go build -ldflags "-s -w -H=windowsgui -extldflags=-static" -o bin/"$BINNAME"_windows_386.exe ../../ && ok
 echo -n -e "|_ bin/$BINNAME""_windows_amd64.exe "
-CGO_ENABLED=1 HOST=x86_64-w64-mingw32 CC=x86_64-w64-mingw32-gcc CXX=x86_64-w64-mingw32-g++ GOOS=windows GOARCH=amd64 go build -o bin/"$BINNAME"_windows_amd64.exe ../../ && ok
+CGO_ENABLED=1 HOST=x86_64-w64-mingw32 CC=x86_64-w64-mingw32-gcc CXX=x86_64-w64-mingw32-g++ GOOS=windows GOARCH=amd64 go build -ldflags "-s -w -H=windowsgui -extldflags=-static" -o bin/"$BINNAME"_windows_amd64.exe ../../ && ok
 
 echo "/ Packaging for debian"
 for arch in arm64 arm amd64 386
