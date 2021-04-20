513ef0a6081b1997c7d0db97e8bf2b4b5f726941
Czarek Nakamoto <cyjan@mrcyjanek.net>
2021-04-20 22:06:35 +0200

    Major bug-fix in build script

--- dist/build.sh
+++ dist/build.sh
@@ -21,18 +21,18 @@ do
     BINNAME="btnet-$name"
     echo "/ Linux builds - addon($name)."
     echo -n -e "|- bin/$BINNAME""_linux_386 "
-    CGO_ENABLED=1 HOST=i686-linux-gnu CC=i686-linux-gnu-gcc CXX=i686-linux-gnu-g++ PKG_CONFIG_PATH=/usr/lib/i686-linux-gnu/pkgconfig/ GOOS=linux GOARCH=386 go build -o bin/"$BINNAME"_linux_386 $addon && ok
+    CGO_ENABLED=1 HOST=i686-linux-gnu CC=i686-linux-gnu-gcc CXX=i686-linux-gnu-g++ PKG_CONFIG_PATH=/usr/lib/i686-linux-gnu/pkgconfig/ GOOS=linux GOARCH=386 go build -o bin/"$BINNAME"_linux_386 $addon/main.go && ok
     echo -n -e "|- bin/$BINNAME""_linux_amd64 "
-    CGO_ENABLED=1 GOOS=linux GOARCH=amd64   go build -o bin/"$BINNAME"_linux_amd64 ../../ && ok
+    CGO_ENABLED=1 GOOS=linux GOARCH=amd64 go build -o bin/"$BINNAME"_linux_amd64 ../../ && ok
     echo -n -e "|- bin/$BINNAME""_linux_arm "
-    CGO_ENABLED=1 HOST=arm-linux-gnueabihf CC=arm-linux-gnueabihf-gcc CXX=arm-linux-gnueabihf-g++ PKG_CONFIG_PATH=/usr/lib/arm-linux-gnueabihf/pkgconfig/ GOOS=linux GOARCH=arm go build -o bin/"$BINNAME"_linux_arm $addon && ok
+    CGO_ENABLED=1 HOST=arm-linux-gnueabihf CC=arm-linux-gnueabihf-gcc CXX=arm-linux-gnueabihf-g++ PKG_CONFIG_PATH=/usr/lib/arm-linux-gnueabihf/pkgconfig/ GOOS=linux GOARCH=arm go build -o bin/"$BINNAME"_linux_arm $addon/main.go && ok
     echo -n -e "\_ bin/$BINNAME""_linux_arm64 "
-    CGO_ENABLED=1 HOST=aarch64-linux-gnu CC=aarch64-linux-gnu-gcc CXX=aarch64-linux-gnu-g++ PKG_CONFIG_PATH=/usr/lib/aarch64-linux-gnu/pkgconfig/ GOOS=linux GOARCH=arm64 go build -o bin/"$BINNAME"_linux_arm64 $addon && ok
+    CGO_ENABLED=1 HOST=aarch64-linux-gnu CC=aarch64-linux-gnu-gcc CXX=aarch64-linux-gnu-g++ PKG_CONFIG_PATH=/usr/lib/aarch64-linux-gnu/pkgconfig/ GOOS=linux GOARCH=arm64 go build -o bin/"$BINNAME"_linux_arm64 $addon/main.go && ok
     echo "/ Windows builds - daemon"
     echo -n -e "|- bin/$BINNAME""_windows_386.exe "
-    CGO_ENABLED=1 HOST=i686-w64-mingw32 CC=i686-w64-mingw32-gcc CXX=i686-w64-mingw32-g++ GOOS=windows GOARCH=386 go build -ldflags "-s -w -H=windowsgui -extldflags=-static" -o bin/"$BINNAME"_windows_386.exe $addon && ok
+    CGO_ENABLED=1 HOST=i686-w64-mingw32 CC=i686-w64-mingw32-gcc CXX=i686-w64-mingw32-g++ GOOS=windows GOARCH=386 go build -ldflags "-s -w -H=windowsgui -extldflags=-static" -o bin/"$BINNAME"_windows_386.exe $addon/main.go && ok
     echo -n -e "|- bin/$BINNAME""_windows_amd64.exe "
-    CGO_ENABLED=1 HOST=x86_64-w64-mingw32 CC=x86_64-w64-mingw32-gcc CXX=x86_64-w64-mingw32-g++ GOOS=windows GOARCH=amd64 go build -ldflags "-s -w -H=windowsgui -extldflags=-static" -o bin/"$BINNAME"_windows_amd64.exe $addon && ok
+    CGO_ENABLED=1 HOST=x86_64-w64-mingw32 CC=x86_64-w64-mingw32-gcc CXX=x86_64-w64-mingw32-g++ GOOS=windows GOARCH=amd64 go build -ldflags "-s -w -H=windowsgui -extldflags=-static" -o bin/"$BINNAME"_windows_amd64.exe $addon/main.go && ok
     for arch in arm64 arm amd64 386
     do
         cd "$builddir"
@@ -41,7 +41,7 @@ do
         "amd64") ar="amd64" ;;
         "arm") ar="armhf" ;;
         "arm64") ar="aarch64" ;;
-        esac        
+        esac
         cp "$builddir/"../debian debian-deb-$name-$arch -r
         cd debian-deb-$name-$arch
         pwd
